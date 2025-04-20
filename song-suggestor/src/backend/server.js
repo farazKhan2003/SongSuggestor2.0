2ee22a3fc5c9e1778f4690ee2346e280a77103cb
farazKhan2003 <kfaraz@hotmail.co.uk>
2025-04-20 16:11:57 +0100

    Adding render application

---
+++ song-suggestor/src/backend/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
